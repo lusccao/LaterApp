27c2d56ab29a283c595793660295272d55dc2b7d
Lucas <lucasarthur7@gmail.com>
2026-01-06 07:58:33 -0300

    Hello World

---
+++ LaterApp/LaterApp/LaterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaterAppApp.swift
+//  LaterApp
+//
+//  Created by Lucas Arthur Chagas Feitosa on 06/01/26.
+//
+
+import SwiftUI
+
+@main
+struct LaterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
